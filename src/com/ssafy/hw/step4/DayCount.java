2d155e973eebf35d04591903ae032a8d0a053bf6
김시하 <hum12393@gmail.com>
2022-07-18 17:24:06 +0900

    Update src/com/ssafy/hw/step4/DayCount.java

--- src/com/ssafy/hw/step4/DayCount.java
+++ src/com/ssafy/hw/step4/DayCount.java
@@ -3,7 +3,23 @@ package com.ssafy.hw.step4;
 public class DayCount {
 
 	public static void main(String[] args) {
-		//코드를 작성하세요.
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("월과 일을 입력하세요.");
+		int month = scan.nextInt();
+		int day = scan.nextInt();
+		
+		int arr[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		int result = 0;
+		result += day;
+		
+		for(int i=0;i<month-1;i++) {
+			result += arr[i];
+			
+		}
+		
+		
+		System.out.println(month + "월 " + day + "일은 " +  result +"번째 날입니다.");
 	}
 
 }
